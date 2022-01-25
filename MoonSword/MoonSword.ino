db53e49aea621f8f2a47ea9463bd3b0285ded54b
DezZzmant <semy91@mail.ru>
2022-01-26 02:27:13 +0500

    almost work

--- MoonSword/MoonSword.ino
+++ MoonSword/MoonSword.ino
@@ -2,13 +2,17 @@
 #include <FastLEDsupport.h> // вкл поддержку
 
 #define LEDPIN 12
-#define Button 5
+#define Button 9
 bool flag = false;
-microLED<30, LEDPIN, -1, LED_WS2818, ORDER_GRB, CLI_AVER> strip;
-
+microLED<30, LEDPIN, MLED_NO_CLOCK, LED_WS2818, ORDER_GRB, CLI_AVER> strip;
+int center = 15;
 void setup() {
   // put your setup code here, to run once:
-
+  //Serial.begin(9600);
+pinMode(Button,INPUT_PULLUP);
+strip.setBrightness(60);
+strip.clear();
+strip.show();
 }
 
 void loop() {
@@ -17,29 +21,41 @@ void loop() {
   
   if (flag == true) // Если флаг Поднят по нажатию кнопки, выводим изображение на ленту
   {
+    Flag_on();
     
   }
   else // Если флаг опущен тушим ленту
   {
-    
+    strip.clear();
+    strip.show();
+    delay(1);
   }
 
 }
 
-void Led_on()
+void Flag_on()
 {
+  for (int i = 0; i < 15; i++) {
+  strip.leds[center - i]=mRed;
+  strip.leds[center + i]= mGreen;
+  strip.show();
+  delay(100);
+  
+  }
   
 }
 
-void Led_off()
+void Flag_off()
 {
-  
+  strip.clear();
+  strip.show();
 }
 
 void Check_button() //Функция проверки нажатия кнопки
 {
- if  (digitalRead(Button) == HIGH) //Если кнопка нажата
+ if  (digitalRead(Button) == LOW) //Если кнопка нажата
  {
-  flag = !flag; // перевернуть флаг на противоположное состояние
+  flag = true; // перевернуть флаг на противоположное состояние
  }
+ else{flag=false;}
 }
